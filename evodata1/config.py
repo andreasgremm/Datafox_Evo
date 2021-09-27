0cf85bb7c085fd64051bac66f2ddc0ea59c254d1
Andreas Gremm <andreas.gremm@t-online.de>
2021-09-27 20:24:14 +0200

    Parameter von Config in das Environment verschoben

--- evodata1/config.py
+++ evodata1/config.py
@@ -1,8 +1,5 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-EVODATA_IP = "192.168.1.35"
-DEBUG_HOST = "127.0.0.1"
-INFLUXDB_HOST = 'innovationv2'
 INFLUXDB_PORT = '8086'
 INFLUXDB_DATABASE = 'evodata'
--- evodata1/evodata/__init__.py
+++ evodata1/evodata/__init__.py
@@ -8,8 +8,11 @@ app = Flask(__name__)
 app.config.from_object("config")
 app.config.update(
     SECRET_KEY=os.getenv('SECRET_KEY'),
+    INFLUXDB_HOST=os.getenv('INFLUXDB_HOST'),
     INFLUXDB_USER=os.getenv('INFLUXDB_USER'),
     INFLUXDB_PASSWORD=os.getenv('INFLUXDB_PASSWORD'),
+    EVODATA_IP=os.getenv('EVODATA_IP'),
+    DEBUG_HOST=os.getenv('DEBUG_HOST'),
 )
 
 from evodata import sichten
